c0e882a588e0bb54f1da251feb27f879090006f7
BAE BYEONGIL <b000202@naver.com>
2023-08-15 21:23:41 +0900

    mainRoom Entrance Alarm

--- public/Javascript/chat.js
+++ public/Javascript/chat.js
@@ -1,14 +1,17 @@
 const socket = new WebSocket('ws://localhost:3000');
+
+const nickname = prompt('Please enter your nickname:');
+
 socket.onopen = function () {
   console.log('Connected');
   socket.send(
     JSON.stringify({
-      event: 'events',
-      data: 'test',
+      event: 'mainRoom',
+      data: nickname,
     }),
   );
   socket.onmessage = function (event) {
     const data = JSON.parse(event.data); // JSON 문자열을 JavaScript 객체로 변환
-    console.log(data); // 실제 데이터 출력
+    console.log(`${data} 님께서 입장하셨습니다.`); // 실제 데이터 출력
   };
 };
--- src/chat/chat.gateway.ts
+++ src/chat/chat.gateway.ts
@@ -11,7 +11,7 @@ export class EventsGateway {
   @WebSocketServer()
   server: Server;
 
-  @SubscribeMessage('events')
+  @SubscribeMessage('mainRoom')
   onEvent(client: any, data: any): WsResponse<any> {
     // 클라이언트가 보낸 데이터를 그대로 다시 클라이언트로 보내기
     return data;
