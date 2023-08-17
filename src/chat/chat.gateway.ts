d821513d656a48a273a7a4e0e971d34a87d28e76
BAE BYEONGIL <b000202@naver.com>
2023-08-18 01:35:49 +0900

    send message to client

--- src/chat/chat.gateway.ts
+++ src/chat/chat.gateway.ts
@@ -4,22 +4,46 @@ import {
   WebSocketServer,
   WsResponse,
 } from '@nestjs/websockets';
-import { Server } from 'ws';
+import { Server, WebSocket } from 'ws';
 
 @WebSocketGateway()
 export class EventsGateway {
   @WebSocketServer()
   server: Server;
 
+  private clients: Set<WebSocket> = new Set();
+
+  handleConnection(client: WebSocket, ...args: any[]) {
+    this.clients.add(client);
+  }
+
+  handleDisconnect(client: WebSocket, ...args: any[]) {
+    this.clients.delete(client);
+  }
+
+  // 클라이언트로 메시지를 보내는 메서드
+  private sendToClient(client: WebSocket, data: object) {
+    if (client.readyState === WebSocket.OPEN) {
+      client.send(JSON.stringify(data));
+    }
+  }
+
+  // 모든 클라이언트에게 메시지를 브로드캐스트하는 메서드
+  private broadcastMessage(data: object) {
+    this.clients.forEach((client) => {
+      this.sendToClient(client, data);
+    });
+  }
+
   @SubscribeMessage('mainRoom')
-  onMainRoomEvent(client: any, data: any): WsResponse<any> {
+  onMainRoomEvent(client: WebSocket, data: string) {
     // 클라이언트가 보낸 데이터를 그대로 다시 클라이언트로 보내기
-    return { event: 'mainRoom', data };
+    this.broadcastMessage({ event: 'mainRoom', data });
   }
 
   @SubscribeMessage('mainChat')
-  onMainChatEvent(client: any, data: any): WsResponse<any> {
+  onMainChatEvent(client: WebSocket, data: string) {
     // 클라이언트가 보낸 데이터를 그대로 다시 클라이언트로 보내기
-    return { event: 'mainChat', data };
+    this.broadcastMessage({ event: 'mainChat', data });
   }
 }
